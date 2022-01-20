537f96c464568b4c42eae2290b5aa33a53a058d8
mariawang2002826 <mariawang2002@gmail.com>
2022-01-20 12:33:53 -0800

    edit files

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,16 +12,21 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int testOpenBracket = markdown.indexOf("[");
-            System.out.println(nextOpenBracket);
-            System.out.println(testOpenBracket);
+            if (nextOpenBracket == -1) {
+                break;
+            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
+            int closeParen = markdown.indexOf(" ", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            System.out.println(toReturn.size()-1);
+            if (closeParen > currentIndex) {
+                currentIndex = closeParen + 1;
+            }
+            else{
+                break;
+            }
         }
-        System.out.println("currentIndex = " + currentIndex);
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
---
+++ test-file-3.md
@@ -0,0 +1,2 @@
+# Test file 3
+[Link](https://github.com/mariawang2002826/markdown-parse)
