564d5c3a9de7cd369dd0a2b25a35d49fe14ff7b2
mariawang2002826 <mariawang2002@gmail.com>
2022-01-20 12:38:28 -0800

    fixed IndexOutOfBound Exception

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,7 +17,7 @@ public class MarkdownParse {
             }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(" ", openParen);
+            int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             System.out.println(toReturn.size()-1);
             if (closeParen > currentIndex) {
