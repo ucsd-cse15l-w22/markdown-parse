492d38314c314a80e9100f28927428e750a5282d
AAP127 <92289585+AAP127@users.noreply.github.com>
2022-01-20 16:50:57 -0800

    Accounted for missing right bracket

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,11 +12,16 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if(nextOpenBracket == -1){
+                System.out.println("Invalid input");
+                break;
+            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            //System.out.println(nextCloseBracket);
         }
         return toReturn;
     }
