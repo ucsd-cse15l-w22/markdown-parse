9631631ef5f8d2d226859d6f1cb213b46e81c207
Ryan Vanny <73187503+Obarquinho@users.noreply.github.com>
2022-01-26 15:25:37 -0800

    second part of lab 4 make junit tests part

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,11 +12,39 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
     @Test
-    public void coolTest() throws IOException {
+    public void cool1Test() throws IOException {
         List<String> cool = List.of("https://something.com", "some-page.html");
         Path fileName = Path.of("test-file.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals(cool, links);
+        
+    }
+    @Test
+    public void cool4Test() throws IOException {
+        List<String> cool = List.of("https://www.ecosia.org/images?q=jesus");
+        Path fileName = Path.of("test4.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(cool, links);
+        
+    }
+    @Test
+    public void cool3Test() throws IOException {
+        List<String> cool = List.of();
+        Path fileName = Path.of("test3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(cool, links);
+        
+    }
+    @Test
+    public void cool2Test() throws IOException {
+        List<String> cool = List.of("https://something.com", "some-page.html");
+        Path fileName = Path.of("yourmom.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(cool, links);
+        
     }
 }
