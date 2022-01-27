871828550bdafd77fc3f29095390901bbd4b030e
PierreBeur <cbeurtheret@ucsd.edu>
2022-01-26 17:10:03 -0800

    Added a test
    javac -cp ".;lib/\*" MarkdownParseTest.java
    
    java -cp ".;lib/\*" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,24 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLink() throws IOException {
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> actual = MarkdownParse.getLinks(contents);
+        List<String> expected = List.of("https://something.com", "some-page.html");
+        assertEquals(expected, actual);
     }
 }
