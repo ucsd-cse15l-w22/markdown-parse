71802498d4546732df2dbdd8bdab00b912a79687
zfxd <zhang_weiyao@yahoo.com.sg>
2022-01-27 10:00:59 -0800

    Add JUnit test
    Commands to test:
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,13 @@
+/** javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+ *  java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+ */
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
--- test-file.md
+++ test-file.md
@@ -1,4 +1,4 @@
 # Title
 
-[a link!](https://something.com)
+[a link![1]](https://something.com)
 [another link!](some-page.html)
