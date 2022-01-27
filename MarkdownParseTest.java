623810aa330c6e55d29a73862b017bb9112aa9d2
PierreBeur <cbeurtheret@ucsd.edu>
2022-01-26 17:21:49 -0800

    Added a test
    javac -cp ".;lib/\*" MarkdownParseTest.java
    
    java -cp ".;lib/\*" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,7 +7,15 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+// javac -cp ".;lib/\*" MarkdownParseTest.java
+// java -cp ".;lib/\*" org.junit.runner.JUnitCore MarkdownParseTest
+
 public class MarkdownParseTest {
+    
+    private static ArrayList<String> getLinks(String filename) throws IOException {
+        return MarkdownParse.getLinks(Files.readString(Path.of(filename)));
+    }
+    
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
@@ -15,10 +23,15 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLink() throws IOException {
-        Path fileName = Path.of("test-file.md");
-	    String contents = Files.readString(fileName);
-        ArrayList<String> actual = MarkdownParse.getLinks(contents);
+        ArrayList<String> actual = getLinks("test-file.md");
         List<String> expected = List.of("https://something.com", "some-page.html");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetLinkTwo() throws IOException {
+        ArrayList<String> actual = getLinks("breaking-test-file.md");
+        List<String> expected = List.of("https://www.google.com");
         assertEquals(expected, actual);
     }
 }
