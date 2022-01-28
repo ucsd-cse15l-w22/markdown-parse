ad1b22832cb346ff7862eab39bf998b480cd03c0
zfxd <zhang_weiyao@yahoo.com.sg>
2022-01-28 08:13:45 -0800

    Og file

--- MarkdownParse-new.java
+++ MarkdownParse-new.java
@@ -12,9 +12,23 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if (nextOpenBracket == -1)
+            {
+                break;
+            }
+            if (nextOpenBracket > 0)
+            {
+                System.out.println("Not 0");
+                if(markdown.charAt(nextOpenBracket-1) == '!')
+                {
+                    currentIndex = nextOpenBracket+1;
+                    continue;
+                }
+            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
