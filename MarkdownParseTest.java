43e375bdb4aaf355c7ebab60abe16535954e206f
heter13 <hespinoza@ucsd.edu>
2022-02-03 10:51:40 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,8 @@
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -15,15 +12,13 @@ public class MarkdownParseTest {
         // Asserting that the left of the comma is equal to the right
         assertEquals(2, 1 + 1);
     }
-  /*
     @Test
-    public void links() throws IOException {
-
-		Path fileName = Path.of("C:\\Users\\heter\\Documents\\GitHub\\markdown-parse, test-file.md");
+    public void test1() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        System.out.println(fileName.toString());
 	    String contents = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(contents);
-        List<String> output = List.of("[]");
-        assertEquals(output, links);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);   
+        List<String> expected = List.of(" ");
+        assertEquals(expected, links);
     }
-    */
 }
