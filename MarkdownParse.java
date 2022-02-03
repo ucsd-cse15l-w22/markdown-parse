0df7287dedadecf1b743030eba9b22235d52548e
mariawang2002826 <97622878+mariawang2002826@users.noreply.github.com>
2022-02-03 11:47:48 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,4 +1,3 @@
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,31 +9,55 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
+        int lastClosedParen = markdown.lastIndexOf(")");
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if (nextOpenBracket == -1) {
-                break;
-            }
+
+            boolean isImage = false;
+
+            if (markdown.indexOf("!", nextOpenBracket-1) == nextOpenBracket - 1) isImage = true;
+            
+
+            if (nextOpenBracket == 0) isImage = false;
+
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
+            int openParen = markdown.indexOf("(", currentIndex);
             int closeParen = markdown.indexOf(")", openParen);
-            if (markdown.substring(nextOpenBracket - 1, nextOpenBracket).equals("!")) {
+
+            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
+		        break;
+	        }
+
+            if (openParen - nextCloseBracket > 2) {
+                currentIndex = markdown.indexOf("[", currentIndex + 1);
+
+                if (currentIndex == lastClosedParen) {
+                    break;
+                } else if (currentIndex < 0) {
+                    break;
+                }
+                continue;
+            }
+
+            
+
+            if(markdown.substring(nextOpenBracket-1, nextOpenBracket).equals("!")) {
                 currentIndex = closeParen + 1;
                 continue;
             }
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            if (closeParen > currentIndex) {
+
+            if (!isImage) toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (closeParen > currentIndex){
                 currentIndex = closeParen + 1;
-            }
-            else{
+            }else {
                 break;
             }
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
