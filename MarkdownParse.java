06693370e06195a96909a8acbded1f1d81f0f539
heter13 <hespinoza@ucsd.edu>
2022-02-03 15:41:22 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -29,7 +29,6 @@ public class MarkdownParse {
             
             currentIndex = closeParen + 1;
         }
-        System.out.println("Don't worry about this");
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
