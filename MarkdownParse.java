2b7c568fb05e690ea8d706ea46de2e0df548e29a
zfxd <zhang_weiyao@yahoo.com.sg>
2022-02-10 09:38:39 -0800

    change makefile

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,7 +9,7 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
-        System.out.println("Adding a line");
+        System.out.println("I made a change!!!!!!!!");
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
