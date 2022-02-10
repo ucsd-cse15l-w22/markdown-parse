21389880f28c14d0c89d1fd586e0c839783990b9
AAP127 <apestonj@gmail.com>
2022-02-09 21:59:37 -0800

    Check MarkdownParse formatting
    Only add a link if its formatting is completely following the
    
    "[text](link)"
    
    format

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,17 +9,45 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
+
+        //Use boolean to determine if formatting is correct
+        //Allow program to continue running but don't add link
+        boolean goodFormat = true;
+
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+
+            //Find next "["
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             if(nextOpenBracket == -1){
-                System.out.println("Invalid input");
+                goodFormat = false;
                 break;
             }
+
+            //Find next "]"
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            if(nextCloseBracket == -1){
+                goodFormat = false;
+                break;
+            }
+
+            //Find next "("
             int openParen = markdown.indexOf("(", nextCloseBracket);
+            if(openParen == -1){
+                goodFormat = false;
+                break;
+            }
+
+            //Find next ")"
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(closeParen == -1){
+                goodFormat = false;
+                break;
+            }
+
+            if(goodFormat){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
