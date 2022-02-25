67c31c64e6cf8ee5952c71c31f22ec4719957b85
B1mendez <Brianmendez2308@gmail.com>
2022-02-25 13:41:44 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,20 +8,17 @@ import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void testLink() throws IOException {
+        String regFile = Files.readString(Path.of("./test-file.md"));
+        String[] regLines = regFile.split("\n");
+        assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(regLines));
     }
 
     @Test
-    public void multiplication() {
-        assertEquals(10, 5*2);
+    public void testSnippet1() throws IOException {
+        String regFile = Files.readString(Path.of("./snippet-1.md"));
+        String[] regLines = regFile.split("\n");
+        assertEquals(List.of("url.com", "`google.com", "google.com", "ucsd.edu"), MarkdownParse.getLinks(regLines));
     }
-    
-    // @Test
-    // public void testLink() throws IOException {
-    //     String regFile = Files.readString(Path.of("./test-file.md"));
-    //     String[] regLines = regFile.split("\n");
-    //     assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(regLines));
-    // }
     
 }
