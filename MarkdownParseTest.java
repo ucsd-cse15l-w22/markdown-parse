78a7b9ef78ac8172c052ce9060935e51917c1b8f
zfxd <zhang_weiyao@yahoo.com.sg>
2022-02-24 21:23:25 -0800

    Snippet 1,2,3
    All expose bugs, none are fixed yet

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -5,9 +5,30 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class MarkdownParseTest {
     @Test
-    public void addition() {
-        assertEquals(4, 1 + 3);
+    public void testSnippet1() throws IOException {
+        String contents = Files.readString(Path.of("./snippet1.md"));
+        List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        String contents = Files.readString(Path.of("./snippet2.md"));
+        List<String> expect = List.of("a.com", "a.com(())", "example.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        String contents = Files.readString(Path.of("./snippet3.md"));
+        List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 }
