b4b26e34365149a4a72e969c2e03237e6519f58f
Joe Politz <joe.politz@gmail.com>
2022-02-28 12:40:31 -0800

    fix indentation

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -83,8 +83,8 @@ public class MarkdownParse {
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		    Path fileName = Path.of(args[0]);
-	      String contents = Files.readString(fileName);
+		Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
