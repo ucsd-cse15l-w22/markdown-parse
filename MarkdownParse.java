478fb01ea152981ee6bbeabddb1846022144ab52
zfxd <zhang_weiyao@yahoo.com.sg>
2022-03-10 14:56:27 -0800

    Infinite loop fix
    Quick fix to do lap report 10

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -28,6 +28,10 @@ public class MarkdownParse {
             }
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (openParen == -1 || closeParen == -1)
+            {
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
